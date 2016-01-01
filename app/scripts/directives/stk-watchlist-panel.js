3520841e11bd6ce30d3fbf13ee95f60b603bc825
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-01-01 15:36:30 +0900

    just yeoman command is executed

---
+++ app/scripts/directives/stk-watchlist-panel.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name stockDogApp.directive:stkWatchlistPanel
+ * @description
+ * # stkWatchlistPanel
+ */
+angular.module('stockDogApp')
+  .directive('stkWatchlistPanel', function () {
+    return {
+      template: '<div></div>',
+      restrict: 'E',
+      link: function postLink(scope, element, attrs) {
+        element.text('this is the stkWatchlistPanel directive');
+      }
+    };
+  });
---
+++ app/scripts/services/watchlist-service.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name stockDogApp.WatchlistService
+ * @description
+ * # WatchlistService
+ * Service in the stockDogApp.
+ */
+angular.module('stockDogApp')
+  .service('WatchlistService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+
+
+
+
+
+
+
+
+
+
+
+
+  });
